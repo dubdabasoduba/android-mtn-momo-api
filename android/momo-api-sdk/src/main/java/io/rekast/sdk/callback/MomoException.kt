ec988109fb1df2575b4bea7337d029f91c40755d
Benjamin Mwalimu <dubdabasoduba@gmail.com>
2024-10-31 04:17:55 +0300

    🔥 Remove redundant code on the API client (#42)
    * 🔥 Remove redundant code on the API client
    
    * 🚚 Rename files and variables correctly
    
    * 🚚 Correct grammar
    
    * 🏗️ Remove call back from the API classes and start using coroutines better
    
    * 💥 Breakign the build because of the hilt dependencies
    
    * Update .gitignore
    
    * Update gradle.properties
    
    * Update gradle.properties
    
    * 🎨 Format the code
    
    * ♻️ Refactor to add HIlt
    
    * 🔥 Delete log files
    
    * 🙈 Ignore log files
    
    * 📝 Set up the documentation system
    
    * 🐛 Bug fixes after adding docusaurus
    
    * 🙈 Add .idea to git ignore
    
    * ♻️ Update the network module for beter readability and logs adittion
    
    * ✨ Add auth interceptors
    
    * ✨ Completed adding HIlt, refrofit and making show the project can query

---
+++ android/momo-api-sdk/src/main/java/io/rekast/sdk/callback/MomoException.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2023, Benjamin Mwalimu Mulyungi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.rekast.sdk.callback
+
+import io.rekast.sdk.model.ErrorResponse
+
+/**
+ * Handles exceptions and messages for the exceptions.
+ *
+ * @property errorResponse The error response associated with the exception.
+ */
+class MomoException : Exception {
+    lateinit var errorResponse: ErrorResponse
+
+    /**
+     * Constructor for MomoException with a message.
+     *
+     * @param message The message associated with the exception.
+     */
+    constructor(message: String?) : super(message)
+
+    /**
+     * Constructor for MomoException with an ErrorResponse.
+     *
+     * @param errorResponse The error response associated with the exception.
+     */
+    constructor(errorResponse: ErrorResponse) : super("${errorResponse.code} : ${errorResponse.message}") {
+        this.errorResponse = errorResponse
+    }
+}
---
+++ android/momo-api-sdk/src/main/java/io/rekast/sdk/callback/TransactionCallback.kt
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2023, Benjamin Mwalimu Mulyungi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.rekast.sdk.callback
+
+import com.google.gson.GsonBuilder
+import io.rekast.sdk.model.ErrorResponse
+import io.rekast.sdk.model.MomoTransaction
+import io.rekast.sdk.utils.Settings
+import io.rekast.sdk.utils.TransactionStatus
+import java.io.IOException
+import okhttp3.ResponseBody
+import okhttp3.ResponseBody.Companion.toResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+/**
+ * Transaction MomoCallback for the MTN MOMO API
+ * The MTN MOMO API returns the a 200 OK even for transfers that failed.
+ * We use [TransactionCallback] to identify the difference between 200 OK and 400
+ * This returns the [MomoTransaction] on the [ResponseBody]
+ */
+class TransactionCallback(
+    private val callback: (momoResponse: MomoResponse<ResponseBody?>) -> Unit
+) : Callback<ResponseBody?> {
+
+    override fun onResponse(call: Call<ResponseBody?>, response: Response<ResponseBody?>) {
+        if (response.isSuccessful) {
+            val momoTransaction: MomoTransaction? = Settings().generateTransactionFromResponse(response)
+            if (momoTransaction?.status != null) {
+                if (momoTransaction.status == TransactionStatus.SUCCESSFUL.name) {
+                    callback.invoke(
+                        MomoResponse.Success(
+                            GsonBuilder().setPrettyPrinting().create().toJson(momoTransaction).toResponseBody()
+                        )
+                    )
+                } else {
+                    val error = "${momoTransaction.reason} : ${momoTransaction.externalId}"
+                    callback.invoke(MomoResponse.Failure(false, MomoException(error)))
+                }
+                return
+            }
+        } else {
+            try {
+                val error = GsonBuilder().create().fromJson(response.errorBody()?.string(), ErrorResponse::class.java)
+                callback.invoke(MomoResponse.Failure(false, MomoException(error)))
+            } catch (e: IOException) {
+                e.printStackTrace()
+                callback.invoke(MomoResponse.Failure(false, MomoException("${response.code()}")))
+            }
+        }
+    }
+
+    override fun onFailure(call: Call<ResponseBody?>, throwable: Throwable) {
+        callback.invoke(MomoResponse.Failure(true, MomoException(throwable.localizedMessage)))
+    }
+}
